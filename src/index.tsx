d224f2b8bc650cc68a9377a70be67167e350e953
Andrii <andrii.khomik@gmail.com>
2024-06-21 14:50:15 +0300

    fix imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
 import { Provider } from "react-redux";
-import { store } from "./store";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import "./index.css";
+import App from "./App";
+import { store } from "./store";
 import ToasterProvider from "./providers/ToasterProvider";
+import "./index.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
